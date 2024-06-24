345871244e5ac6eb97e59b3a374e5f55ab0ab8b1
Ee Durbin <ewdurbin@gmail.com>
2024-06-24 13:57:19 -0400

    be quiet!

--- purge.sh
+++ purge.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find /usr/share/nginx/html/ | sed 's:/usr/share/nginx/html::g' | xargs -I{} sh -c "echo https://bootstrap.pypa.io{}; curl -XPURGE https://bootstrap.pypa.io{}; echo;"
+find /usr/share/nginx/html/ | sed 's:/usr/share/nginx/html::g' | xargs -I{} sh -c "echo https://bootstrap.pypa.io{}; curl --silent -XPURGE https://bootstrap.pypa.io{}; echo;"
